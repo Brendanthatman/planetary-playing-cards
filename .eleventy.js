daef497ad197c958e038bd493ba78256dcc17dcc
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-02-05 20:35:49 +0000

    input information for cards

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles");
 
   eleventyConfig.addShortcode("planetCard", function(planet, au){
-    return `  
-      <div class="planet_drawing" id="${planet}_drawing"></div>
-      <h2 class="card_title">${planet}</h2>
-      <p class="au_card">AU ${au}</p>
+    return `
+      <div class="${planet}_card">  
+        <div class="planet_drawing" id="${planet}_drawing"></div>
+        <h2 class="card_title">${planet}</h2>
+        <p class="au_card">AU ${au}</p>
+      </div>
     `;
   });
 };
